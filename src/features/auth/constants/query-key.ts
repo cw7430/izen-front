62631248d522df162499dc4354d6661b15d9c7ac
cw7430 <cw282@naver.com>
2026-08-07 11:19:33 +0900

    query-keys 수정

--- src/features/auth/constants/query-key.ts
+++ src/features/auth/constants/query-key.ts
@@ -1,8 +1,8 @@
 const ALL_KEY = 'auth';
 
 export const AUTH_KEYS = {
-  all: [ALL_KEY] as const,
-  login: [ALL_KEY, 'login'] as const,
-  logout: [ALL_KEY, 'logout'] as const,
-  refresh: [ALL_KEY, 'refresh'] as const,
+  all: [ALL_KEY],
+  login: [ALL_KEY, 'login'],
+  logout: [ALL_KEY, 'logout'],
+  refresh: [ALL_KEY, 'refresh'],
 } as const;
